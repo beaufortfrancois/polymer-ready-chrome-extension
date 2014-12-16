0838b96e261114c02e3ffa541413e799963f7dd2
Jake Johnson <jake@fastdivision.com>
2014-12-15 23:05:46 -0800

    Use Polymer pink overlays for highlighting instead of outlines. Make sure highlights are removed after the popup is closed or an element is clicked

--- src/polymer-ready.js
+++ src/polymer-ready.js
@@ -1,4 +1,10 @@
 var customElements, originalOutline;
+var removeOverlays = function() {
+  var overlays = document.querySelectorAll('.polymer-ready-overlay');
+  [].forEach.call(overlays, function(el) {
+    document.body.removeChild(el);
+  });
+};
 
 window.addEventListener('polymer-ready', function(event) {
   // Send message to background.js when polymer-ready is fired.
@@ -8,7 +14,6 @@ window.addEventListener('polymer-ready', function(event) {
 chrome.runtime.onConnect.addListener(function(port) {
   port.onMessage.addListener(function(msg) {
     switch(msg.action) {
-
       case 'get-custom-elements':
         // Get all custom elements.
         customElements = Array.prototype.slice.call(document.querySelectorAll('* /deep/ *')).filter(function(element) {
@@ -17,11 +22,6 @@ chrome.runtime.onConnect.addListener(function(port) {
         if (customElements.length === 0) {
           return;
         }
-        // Save original outline styles for each custom elements.
-        originalOutline = [];
-        customElements.forEach(function(el, i) {
-          originalOutline[i] = el.style.outline;
-        });
         // Send unique sorted custom elements localName to popup.js.
         var customElementsNames = customElements.map(function(el) { return el.localName }).sort().filter(function(el,i,a) { return i==a.indexOf(el); });
         port.postMessage({ customElements: customElementsNames });
@@ -29,15 +29,28 @@ chrome.runtime.onConnect.addListener(function(port) {
 
       case 'show-custom-elements':
         customElements.filter(function(el) { return el.localName === msg.filter }).forEach(function(element) {
-          element.style.setProperty('outline', '1px dashed #3e50b4');
+          var overlay = document.createElement('div');
+          var rect = element.getBoundingClientRect();
+          var bodyRect = document.body.getBoundingClientRect();
+          var offset = { top: rect.top - bodyRect.top, left: rect.left - bodyRect.left };
+          overlay.setAttribute('class', 'polymer-ready-overlay');
+          overlay.style.cssText = "position: absolute; "
+                                + "top: " + offset.top + "px; "
+                                + "left: " + offset.left + "px; "
+                                + "width: " + rect.width + "px; "
+                                + "height: " + rect.height + "px; "
+                                + "background: rgba(255, 64, 129, 0.5); "
+                                + "z-index: 2147483647";
+          document.body.appendChild(overlay);
         });
         break;
 
       case 'hide-custom-elements':
-        customElements.forEach(function(element, i) {
-          element.style.outline = originalOutline[i];
-        });
+        removeOverlays();
         break;
     }
+  });
+  port.onDisconnect.addListener(function() {
+    removeOverlays();
   });
 });
